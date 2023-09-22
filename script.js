34e9a0920a58f0a522ccaf956a0c74ffd2f4aa80
Ankit-GLB <ankitraj.ms11@gmail.com>
2023-09-23 04:15:51 +0530

    repair

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const http = require("http");
 const port = 8000;
 const hostname = "localhost";
 
-
+console.log("temp");
 
 const home = fs.readFileSync("./profession.html","utf-8");
 
@@ -13,7 +13,7 @@ const server = http.createServer((req,res)=>{
         res.end(home);
     }
     else{
-        res.end("./404.html");
+        res.end("404 Not Found");
     }
 }).listen(port,hostname,()=>{
     console.log(`working at http://${hostname}:${port}`);
