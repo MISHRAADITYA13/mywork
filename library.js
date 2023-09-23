cd58d1d09fdc1457b780a63c7eee001d6a1d6f65
Ankit-GLB <ankitraj.ms11@gmail.com>
2023-09-23 13:00:04 +0530

    library page complete

--- library.js
+++ library.js
@@ -27,3 +27,28 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
+
+  document.getElementById('logo_btn').addEventListener('click', function() {
+    console.log("logo_btn working")
+    var localFilePath = './mainpage.html';
+    // window.open(localFilePath, '_blank');
+    window.location.href = localFilePath;
+  });
+  document.getElementById('home_btn').addEventListener('click', function() {
+    console.log("home_btn working")
+    var localFilePath = './mainpage.html';
+    // window.open(localFilePath, '_blank');
+    window.location.href = localFilePath;
+  });
+  document.getElementById('library_btn').addEventListener('click', function() {
+    console.log("library_btn working")
+    var localFilePath = './library.html';
+    // window.open(localFilePath, '_blank');
+    window.location.href = localFilePath;
+  });
+  document.getElementById('feedback_btn').addEventListener('click', function() {
+    console.log("feedback_btn working")
+    var localFilePath = './feedback.html';
+    // window.open(localFilePath, '_blank');
+    window.location.href = localFilePath;
+  });
